7062d1a35e5972dfad2505853d6f9c96bbbf05a9
Martijn Braam <martijn@brixit.nl>
2024-05-27 12:45:23 +0200

    Add the parts subpackage to the pypi package and bump to 1.4.1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='turbocase',
-      version='1.4',
+      version='1.4.1',
       description='Generate an OpenSCAD case template from a KiCAD PCB',
       url='https://git.sr.ht/~martijnbraam/turbocase',
       author='Martijn Braam',
       author_email='martijn@brixit.nl',
-      packages=['turbocase'],
+      packages=['turbocase', 'turbocase.parts'],
       install_requires=['sexpdata'],
       entry_points={
           'console_scripts': ['turbocase=turbocase.__main__:main'],
