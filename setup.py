877e86512b941f9768c39703d9ad02471c00eba0
Martijn Braam <martijn@brixit.nl>
2024-06-20 16:51:06 +0200

    Bump version to 1.7.0

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
 setup(name='turbocase',
-      version='1.6.1',
+      version='1.7.0',
       description='Generate an OpenSCAD case template from a KiCAD PCB',
-      url='https://git.sr.ht/~martijnbraam/turbocase',
+      url='https://turbocase.org',
       author='Martijn Braam',
       author_email='martijn@brixit.nl',
       packages=['turbocase', 'turbocase.parts'],
       install_requires=['sexpdata'],
+      project_urls={
+          'Source': 'https://git.sr.ht/~martijnbraam/turbocase',
+          'Tracker': 'https://todo.sr.ht/~martijnbraam/turbocase',
+      },
       entry_points={
           'console_scripts': ['turbocase=turbocase.__main__:main'],
       })
